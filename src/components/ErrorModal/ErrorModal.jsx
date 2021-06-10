31f458cae04228ee326c0e46a6bc4ac66bad205c
Hrishikesh Prabhu <hprabhu@bgeltd.com>
2021-06-10 10:29:47 +0530

    replaced useState with useRef

--- src/components/ErrorModal/ErrorModal.jsx
+++ src/components/ErrorModal/ErrorModal.jsx
@@ -1,23 +1,35 @@
+import React , {Fragment} from 'react';
+import ReactDOM from 'react-dom'
 import classes from './ErrorModal.module.css';
 import Button from '../Button/Button';
 import Card from '../Card/Card';
 
+const BackDrop = (props) => {
+    return <div className={classes.backdrop} onClick={props.onConfirm}></div>
+}
+
+const Overlay = (props) => {
+    return (
+        <Card className={classes.modal}>
+            <header className={classes.header}>
+                <h2>Input Validation!</h2>
+            </header>
+            <p className={classes.content}>
+                {props.onMessage}
+            </p>
+            <div className={classes.actions}>
+                <Button type="button" onClick={props.onConfirm}>Ok</Button>
+            </div>
+        </Card>
+    )
+}
+
 const ErrorModal = (props) => {
     return (
-        <div>
-            <div className={classes.backdrop} onClick={props.onConfirm}></div>
-            <Card className={classes.modal}>
-                    <header className={classes.header}>
-                        <h2>Input Validation!</h2>
-                    </header>
-                    <p className={classes.content}>
-                        {props.onMessage}
-                    </p>
-                    <div className={classes.actions}>
-                        <Button type="button" onClick={props.onConfirm}>Ok</Button>
-                    </div>
-            </Card>
-        </div>            
+        <Fragment>
+            {ReactDOM.createPortal(<BackDrop onConfirm={props.onConfirm}/>, document.getElementById("backdrop-root"))}
+            {ReactDOM.createPortal(<Overlay onMessage={props.onMessage} onConfirm={props.onConfirm}/>, document.getElementById("overlay-root"))}        
+        </Fragment>            
     )
 }
 
--- src/components/UserInput/UserInput.jsx
+++ src/components/UserInput/UserInput.jsx
@@ -1,46 +1,41 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import userInputStyles from './UserInput.module.css';
 import Button from '../Button/Button';
 import ErrorModal from '../ErrorModal/ErrorModal';
 import Card from '../Card/Card';
 
 const UserInput = (props) => {
-    const [userName, setUserName] = useState('');
-    const [userAge, setUserAge] = useState('');
     const [error, setError] = useState();
     const [message, setMessage] = useState('');
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
     
-    const userNameHandler = (event) => {
-        setUserName(event.target.value);
-    }
-    const userAgeHandler = (event) => {
-        setUserAge(event.target.value);
-    }
     const submitHandler = (event) => {
         event.preventDefault();
-        if(userName === "" && userAge === "") { 
+        const userNameRef = nameInputRef.current.value;
+        const ageRef = ageInputRef.current.value;
+        if(userNameRef === "" && ageRef === "") { 
             setMessage("Please enter Username and Age");
             setError(true);
             return
-        } else if (+userAge < 1){
+        } else if (+ageRef < 1){
             setMessage("Please enter valid age (> 0)");
             setError(true);
             return
         }
         props.onUpdateList({
             id: Math.floor((Math.random() * 100) + 1),
-            name: userName,
-            age: userAge
+            name: userNameRef,
+            age: ageRef
         });  
-        setUserName('');
-        setUserAge('');      
+        nameInputRef.current.value = "";
+        ageInputRef.current.value = "";     
     }
     const closeModal = () => {
-        console.log("clicked");
         setError(false);
     }
     return(
-        <div>
+        <React.Fragment>
             {                
                 error && 
                 <ErrorModal onConfirm={closeModal} onError={error} onMessage={message}/>
@@ -50,16 +45,16 @@ const UserInput = (props) => {
                 <form onSubmit={submitHandler}>
                     <div className={userInputStyles.input}>
                         <label>Username</label>
-                        <input type="text" onChange={userNameHandler} value={userName}/>
+                        <input type="text" ref={nameInputRef}/>
                     </div>
                     <div className={userInputStyles.input}>
                         <label>Age</label>
-                        <input type="number" onChange={userAgeHandler} value={userAge}/>
+                        <input type="number" ref={ageInputRef}/>
                     </div>
                     <Button type="submit">Add User</Button>
                 </form>
             </Card>
-        </div>
+        </React.Fragment>
     )
 }
 
